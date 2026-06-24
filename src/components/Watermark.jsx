b012e57dfb3d50a286f5955d03c6010c278f1d12
Zayesha <hassanhusnain06@gmail.com>
2026-06-24 16:01:23 -0700

    Updated all slides and project files

--- src/components/Watermark.jsx
+++ src/components/Watermark.jsx
@@ -2,7 +2,7 @@ const Watermark = () => {
   return (
     // 'absolute' use kiya taake ye card ke andar rahe, screen se bahar na bhage
     // 'top-4 right-4' se ye mobile screen par top-right corner mein fix rahega
-    <div className="absolute top-3 right-122 z-50 text-[#8B0000] font-serif font-bold text-sm md:text-base opacity-70 select-none pointer-events-none tracking-widest">
+    <div className="absolute top-5 right-200 z-50 text-[#8B0000] font-serif font-bold text-sm md:text-base opacity-70 select-none pointer-events-none tracking-widest">
       #ZA
     </div>
   );
