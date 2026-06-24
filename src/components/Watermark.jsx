585b94038e1a3039e069800406bed4bb300076d8
Zayesha <hassanhusnain06@gmail.com>
2026-06-24 16:14:59 -0700

    Updated all slides and project files

--- src/components/Watermark.jsx
+++ src/components/Watermark.jsx
@@ -2,7 +2,7 @@ const Watermark = () => {
   return (
     // 'absolute' use kiya taake ye card ke andar rahe
     // 'left-4' ka matlab hai left edge se thora sa andar (Mobile ke liye perfect)
-    <div className="absolute top-4 left-3 z-50 text-[#8B0000] font-serif font-bold text-sm md:text-base opacity-70 select-none pointer-events-none tracking-widest">
+    <div className="absolute top-5 left-3 z-50 text-[#8B0000] font-serif font-bold text-sm md:text-base opacity-70 select-none pointer-events-none tracking-widest">
       #ZA
     </div>
   );
